a29013083fd16c89e03b9cbe77169f135dd6fe20
Robertas <meska.lt@gmail.com>
2015-12-12 17:44:21 +0200

    Initial main source file commit.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "BMP24.h"
+#define RESULT_IMAGE_HEIGHT 600
+#define RESULT_IMAGE_WIDTH 600
+#define BORDER_WIDTH 50
+
+int main() {
+	std::cout << "Programa darbą pradėjo.\n";
+	std::cout << "Programa darbą baigė.\n";
+}
