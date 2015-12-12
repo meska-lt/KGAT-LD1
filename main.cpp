1808f0c7175b7cbce2cf7372c2825c78e92d5373
Robertas <meska.lt@gmail.com>
2015-12-12 18:03:45 +0200

    Added more-or-less proper documentation to main class.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+/**
+	KGAT-15 LD1
+	main.cpp
+	Purpose: draw author's initials (character symbols have to be filled with gradient).
+
+	@author: Robertas Kvietkauskas
+	@version: 0.1.0
+
+ */
+
 #include <iostream>
 #include "BMP24.h"
+
 #define RESULT_IMAGE_HEIGHT 600
 #define RESULT_IMAGE_WIDTH 600
 #define BORDER_WIDTH 50
