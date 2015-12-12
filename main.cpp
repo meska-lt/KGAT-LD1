c984fe369f3dec1c713dea8060f134f3a3c54365
Robertas <meska.lt@gmail.com>
2015-12-12 18:46:02 +0200

    Gradient drawing part done.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 	Purpose: draw author's initials (character symbols have to be filled with gradient).
 
 	@author: Robertas Kvietkauskas
-	@version: 0.1.0
+	@version: 0.2.0
 
  */
 
@@ -13,9 +13,31 @@
 
 #define RESULT_IMAGE_HEIGHT 600
 #define RESULT_IMAGE_WIDTH 600
+#define RESULT_FILENAME "result.bmp"
 #define BORDER_WIDTH 50
+
+void fillBitmapWithGradient(BMP24 &bitmap, int bitmapWidth, int bitmapHeight) {
+	std::cout << "void fillBitmapWithGradient(BMP24, int, int);\n";
+
+	for (int x = 0; x < RESULT_IMAGE_WIDTH; x++) {
+		for (int y = 0; y < RESULT_IMAGE_HEIGHT; y++) {
+			char redValue = 255-255.0 * y / RESULT_IMAGE_HEIGHT;
+			char greenValue = 255-255.0 * y / RESULT_IMAGE_WIDTH;
+			char blueValue = 0 + (255.0 * y / RESULT_IMAGE_WIDTH);
+
+			bitmap.dekTaska(x, y, redValue, greenValue, blueValue);
+		}
+	}
+}
 
 int main() {
 	std::cout << "Programa darbą pradėjo.\n";
+
+	BMP24 resultImage = BMP24(RESULT_IMAGE_WIDTH,RESULT_IMAGE_HEIGHT);
+
+	fillBitmapWithGradient(resultImage, RESULT_IMAGE_HEIGHT, RESULT_IMAGE_HEIGHT);
+
+	resultImage.rasykIByla(RESULT_FILENAME);
+
 	std::cout << "Programa darbą baigė.\n";
 }
